28fc91ac4f7dcc225cb3f40bc7a1e2039f831b57
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2022-02-20 11:28:13 +0530

    heroku deployment commit

--- rasafrontend/rasafrontend/settings.py
+++ rasafrontend/rasafrontend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'frontendchat/static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
